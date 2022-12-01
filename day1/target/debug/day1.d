e246c8d7b73e5abf34609171f6e62acdd0c41c77
Matheus Filipe dos Santos Reinert <matheus.reinert@protonmail.com>
2022-12-01 19:25:01 -0300

    Remove stuff

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/shinobu/advent-2022/day1/target/debug/day1: /home/shinobu/advent-2022/day1/src/main.rs
--- day1/target/debug/deps/day1-bb42ab0b3cbb70af.d
+++
@@ -1,5 +0,0 @@
-/home/shinobu/advent-2022/day1/target/debug/deps/day1-bb42ab0b3cbb70af: src/main.rs
-
-/home/shinobu/advent-2022/day1/target/debug/deps/day1-bb42ab0b3cbb70af.d: src/main.rs
-
-src/main.rs:
